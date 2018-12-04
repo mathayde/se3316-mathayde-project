fc9d38600f0c5d3004553a62295e47eac8b0f5ea
Matt Athayde <mathayde@uwo.ca>
2018-12-04 23:37:51 +0000

    Initial Angular-Firebase App Setup

---
+++ project/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+        apiKey: "AIzaSyAkdUGk1uSdSApNAH91eyxYuFybaOZ0_18",
+        authDomain: "project-ae69f.firebaseapp.com",
+        databaseURL: "https://project-ae69f.firebaseio.com",
+        projectId: "project-ae69f",
+        storageBucket: "project-ae69f.appspot.com",
+        messagingSenderId: "1094308988041"
+  }
+};
